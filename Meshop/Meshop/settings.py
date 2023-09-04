ef57cef3ba9df2ad4d4a1c2000fab863c52ec9b1
thomas1502 <thomas1502@mesoamericana.edu.gt>
2023-09-04 14:07:28 -0600

    Conexion de Firebase-Django Paso3

--- Meshop/Meshop/settings.py
+++ Meshop/Meshop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'clear_cache',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
